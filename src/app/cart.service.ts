e83f9f5437ea181a3cf61477ae7f3c154e640f87
MathiasHeimbach <60534997+MathiasHeimbach@users.noreply.github.com>
2023-04-15 10:01:22 +0200

    add cart-service

---
+++ src/app/cart.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Product } from './products';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  item: Product[] = [];
+
+  constructor() { }
+}
